54ee6788f500d1f819cef96e048249e3f7dc52a2
andres <jorquera.ad@gmail.com>
2025-08-15 19:29:34 +0200

    update

--- source/infrastructure/prebid_server/prebid_server_stack.py
+++ source/infrastructure/prebid_server/prebid_server_stack.py
@@ -48,7 +48,7 @@ class PrebidServerStack(SolutionStack):
             id="SSLCertificateARN",
             description="The ARN of an SSL certificate in AWS Certificate Manager associated with a domain name. This field is only required if InstallCloudFrontAndWAF is set to \"No\".",
             type="String",
-            default=globals.SSL_CERTIFICATE_ARN,
+            default="",
         )
         self.solutions_template_options.add_parameter(deploy_cloudfront_and_waf_param, label="",
                                                       group="Content Delivery Network (CDN) Settings")
